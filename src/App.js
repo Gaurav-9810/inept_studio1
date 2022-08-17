f69cc91031c979f0cfc0b9c0eba303bcfbf7157f
guarav <gauravgangola444@gmail.com>
2022-08-17 17:43:20 +0530

    techused

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import WebD from './component/Web_Dev/Web_Dev.js';
 import UI from './component/UI_Desg/UI_Desg.js';
 import App_Dev from './component/App_Dev/App_Dev.js';
 import Two from './component/Two_D_animation/Two';
+import Home from './component/Home-page/Home.js';
+import Game from './component/game-dev/Game_dev.js';
+import Digital from './component/digital/Digital.js';
 
 
 
