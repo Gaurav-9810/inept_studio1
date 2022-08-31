94708dd57ec51f13e0a670cd66504e5ac988d66a
guarav <gauravgangola444@gmail.com>
2022-08-31 15:42:23 +0530

    "change"

--- src/component/Home-page/Faq.js
+++ src/component/Home-page/Faq.js
@@ -4,10 +4,11 @@ import "../Two_D_animation/Faq.css";
 import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 
+
 import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
 import ExpandLessOutlinedIcon from '@mui/icons-material/ExpandLessOutlined'; 
 
-function Faq() {
+function Faq({home}) {
       const[show,setshow]=useState('1');
 
       const handleClick=(c)=>{
@@ -39,6 +40,7 @@ function Faq() {
                   </AnimationOnScroll>
             </div>
       {/* </AnimationOnScroll> */}
+      
             <div className=' lg:w-2/5 flex flex-col justify-center lg:ml-6 '>
       <AnimationOnScroll  animateOnce={true}  animateIn="animated animatedFadeInUp sideanimation2">
                  
@@ -46,10 +48,10 @@ function Faq() {
                         <div className='bg-violet-700  rounded-md pb-px' >
                 
                               
-                            <h2  className='pl-2 font-semibold  text-left md:pl-4 text-white py-2 flex justify-between  ' >How Much Does Business logo cost ? {show=='1'?<span className=' mr-4  ' ><ExpandLessOutlinedIcon/></span>:<span  className='mr-4 z-30'><ExpandMoreOutlinedIcon/></span>}</h2>  
+                            <h2  className='pl-2 font-semibold  text-left md:pl-4 text-white py-2 flex justify-between  ' >{home[0].question} {show=='1'?<span className=' mr-4  ' ><ExpandLessOutlinedIcon/></span>:<span  className='mr-4 z-30'><ExpandMoreOutlinedIcon/></span>}</h2>  
                        
                        
-                        { show=='1' &&  <p  className=' mb-2  faq-question rounded-b-md  py-2 px-3 bg-white text-left md:px-8 md:py-4'>It entirely depends on your product/ service and the kind of image that you want to build for your brand. We also need to keep in mind the product label cost and future copyright issues regarding it.</p>
+                        { show=='1' &&  <p  className=' mb-2  faq-question rounded-b-md  py-2 px-3 bg-white text-left md:px-8 md:py-4'>{home[0].answer}</p>
                         }
                         
                        
@@ -59,17 +61,17 @@ function Faq() {
                   </AnimationOnScroll>
                   <div className='lg:mr-4 lg:ml-4 mb-4 cursor-pointer rounded-md bg-violet-700' onClick={()=>handleClick('2')} >
                         <div className=' rounded-md shadow-md '>
-                            <h2 className='pl-2 font-semibold  text-left md:pl-4 text-white py-2 flex justify-between'>Why are logos important ? {show=='2'?<span className=' mr-4'><ExpandLessOutlinedIcon/></span>:<span className='mr-4'><ExpandMoreOutlinedIcon/></span>}</h2>  
+                            <h2 className='pl-2 font-semibold  text-left md:pl-4 text-white py-2 flex justify-between'>{home[1].question} {show=='2'?<span className=' mr-4'><ExpandLessOutlinedIcon/></span>:<span className='mr-4'><ExpandMoreOutlinedIcon/></span>}</h2>  
                        
-                      { show=='2' &&   <p  className='mb-2 rounded-b-md  py-2 px-3 bg-white text-left md:px-8 md:py-4'> A logo creates a brand identity and a sense of security that is usually needed to ensure customers about the brand's credibility.</p>
+                      { show=='2' &&   <p  className='mb-2 rounded-b-md  py-2 px-3 bg-white text-left md:px-8 md:py-4'> {home[1].answer}</p>
                         }
                         </div>
                   </div>
                   <div className='lg:mr-4 lg:ml-4 mb-3 cursor-pointer rounded-md bg-violet-700' onClick={()=>handleClick('3')} >
                         <div className=' rounded-md shadow-md '>
-                            <h2 className='pl-2 font-semibold  text-left md:pl-4 text-white py-2 flex justify-between'>Can you help me rank in google ? {show=='3'?<span className=' mr-4'><ExpandLessOutlinedIcon/></span>:<span className='mr-4'><ExpandMoreOutlinedIcon/></span>}</h2>  
+                            <h2 className='pl-2 font-semibold  text-left md:pl-4 text-white py-2 flex justify-between'>{home[2].question} {show=='3'?<span className=' mr-4'><ExpandLessOutlinedIcon/></span>:<span className='mr-4'><ExpandMoreOutlinedIcon/></span>}</h2>  
                        
-                      { show=='3' &&   <p  className='mb-2 rounded-b-md  py-2 px-3 bg-white text-left md:px-8 md:py-4'>Yes we can! Higher ranking in google mainly depends upon your On-Site SEO, LSI keywords on your page and your bounce rate. But don't worry about the technicalities, our SEO team has got you covered!</p>
+                      { show=='3' &&   <p  className='mb-2 rounded-b-md  py-2 px-3 bg-white text-left md:px-8 md:py-4'>{home[2].answer}</p>
                         }
                         </div>
                   </div>
---
+++ src/component/Home-page/faqData.js
@@ -0,0 +1,23 @@
+export const home=[
+      {question:"How Much Does Business logo cost ?",
+      answer:"It entirely depends on your product/ service and the kind of image that you want to build for your brand. We also need to keep in mind the product label cost and future copyright issues regarding it."
+      },
+      {question:"Why are logos important ?",
+      answer:"A logo creates a brand identity and a sense of security that is usually needed to ensure customers about the brand's credibility."
+      },
+      {question:"Can you help me rank in google ?",
+      answer:"Yes we can! Higher ranking in google mainly depends upon your On-Site SEO, LSI keywords on your page and your bounce rate. But don't worry about the technicalities, our SEO team has got you covered!"
+      }
+]
+
+export const threeD=[
+      {question:"How Much Does Business logo cost ?",
+      answer:"It entirely depends on your product/ service and the kind of image that you want to build for your brand. We also need to keep in mind the product label cost and future copyright issues regarding it."
+      },
+      {question:"Why are logos important ?",
+      answer:"A logo creates a brand identity and a sense of security that is usually needed to ensure customers about the brand's credibility."
+      },
+      {question:"Can you help me rank in google ?",
+      answer:"Yes we can! Higher ranking in google mainly depends upon your On-Site SEO, LSI keywords on your page and your bounce rate. But don't worry about the technicalities, our SEO team has got you covered!"
+      }
+]
